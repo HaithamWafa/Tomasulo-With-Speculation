79250c1997f95cfcaa31366dbd513001a1db3100
Mustafa Mahmoud <mustafamohammed_auc@aucegypt.edu>
2019-12-10 00:16:08 +0200

    ya rabbb

--- register.py
+++ register.py
@@ -1,6 +1,6 @@
 class register:
     def __init__(self):
-        self.data = None
+        self.data = 0
         self.tag = None
 
     def issue(self, tag):
@@ -12,7 +12,7 @@ class register:
 
 class RegFile:
 	def __init__(self):
-		self.R = [register() for i in range(7)]
+		self.R = {'R0': register(), 'R1': register(), 'R2': register(), 'R3': register(), 'R4': register(), 'R5': register(), 'R6': register(), 'R7': register()}
 
 	def issue(self, RegNo, tag):
 		self.R[RegNo].issue(tag)
