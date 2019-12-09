020574eeaa1988a6091f5f087d71d1d9fad16d47
HaithamWafa <49447918+HaithamWafa@users.noreply.github.com>
2019-12-09 18:51:01 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,77 @@ Created on Sat Nov 16 11:24:17 2019
 
 @author: Haitham Samir
 """
+class ROBentry:
+    def __init__(self):
+        self.ROBentry = None
+        self.empty = True
+        self.done = False
+        self.tag = None
+        self.result = None
 
+    def WriteInst(self, instruction, InstNumber):
+     	self.ROBentry = instruction
+     	self.empty = False
+     	self.tag = InstNumber
+
+    def ClearEntry(self):
+        self.ROBentry = None
+        self.empty = True
+        
+    def InstCompleted(self, result):
+    	self.done = True
+    	self.result = result
+
+    def print_entry(self):
+    	print(self.ROBentry, '\t', self.empty, '\t', self.done, '\t', self.tag, '\t', self.result)
+
+class ROB:
+	NofEntries = 0
+	def __init__(self, NofEntries):
+		self.entries = [ROBentry() for i in range(NofEntries)]
+		self.head = 0
+		self.tail = 0
+		self.full = False
+		self.NofEntries = NofEntries
+
+	def write(self, instruction, InstNumber):
+		if self.entries[self.head].empty:
+			self.entries[self.head].WriteInst(instruction, InstNumber)
+			if self.head < self.NofEntries:
+				self.head += self.head
+			else:
+				self.head = 0
+		else:
+			self.full = True
+			print("ROB is full, stop issuing")
+
+	def CompleteInst(self, result, InstNumber):
+		for entry in self.entries:
+			if entry.tag == InstNumber:
+				entry.InstCompleted(result)
+				break
+
+	def commit(self):
+		if self.entries[self.tail].empty == False and self.entries[self.tail].done == True:
+			self.entries[self.tail].ClearEntry()
+			self.full = False
+			if self.tail < self.NofEntries:
+				self.tail += self.tail
+			else:
+				self.tail = 0
+		else:
+			print("Nothing to commit.")
+
+	def empty_entries(self):
+		how_many = 0
+		for entry in self.entries:
+			if entry.empty:
+				how_many += how_many
+		return how_many
+
+	def print_all_entries(self):
+		for entry in self.entries:
+			entry.print_entry()
 
 
 class instruction:
@@ -15,7 +85,10 @@ class instruction:
             self.source2 = 'R0'
             self.immediate = 0
             self.tag = 0
-            
+            self.functional_unit = 'NA'
+            self.issued = 'NO'
+     
+          
             
 def Import_Instruction_File():
 #   instFile= open("Instructions.txt","w+")       #opening instructions file for writing
@@ -40,6 +113,7 @@ def Parse_Instructions(instructions):
     for i in range(len(instructions)):   #parsing the instructions read
        inst[i]=instructions[i].split()
        instructions[i]=instruction() 
+       instructions[i].tag=i   #setting the instruction order 
        instructions[i].instType=inst[i][0]   #setting the type of instruction
        if(inst[i][0]== 'ADD' or inst[i][0]== 'SUB' or inst[i][0]== 'NAND' or inst[i][0]== 'MUL'):
           instructions[i].destination=inst[i][1]
@@ -95,9 +169,55 @@ def main():
    instructions = Import_Instruction_File() #imports instructions as a list of strings
    Parse_Instructions(instructions)
    
-   #print(instructions[1].destination)   #used for testing  
+   print(instructions[1].destination)   #used for testing  
    
-   
+ #  issue_cycles=0
+   #issuing loop
+#   for i in range(len(instructions)-1):
+#       if(instructions[i].functional_unit.reservation_station.busy == 'NO' and instructions[i+1].functional_unit.reservation_station.busy and ROB.available_entries == 2):
+#           instructions[i].issued = 'YES'
+#           instructions[i+1].issued = 'YES'
+#           issue_cycles=issue_cycles+1
+  # I = instruction()
+   R = ROB(16)
+   R.write(instructions[1 ].destination, 1)
+   R.CompleteInst(33, 1)
+   #R.commit()
+   R.print_all_entries()
 
 if __name__ == '__main__':
     main()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
